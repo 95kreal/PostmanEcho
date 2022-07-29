c4f30726209653e9cd4eb47989ba0a470788afbc
95kreal <95k.real@gmail.com>
2022-07-29 10:17:27 +0300

    second commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PostmanEchoTest {
+
+    @Test
+    void should1() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Postman test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(300)
+                .body("data", equalTo("Postman test"))
+                .body("data", notNullValue())
+        ;
+    }
+}
